5b7f35aeb9572b7ca4c66be35893bac7ee618801
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-04-17 12:18:36 +0900

    🎁 : 매직넘버 추가 및 여러가지 콘솔 메서드를 사용해서 스네이크 움직임 구현

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,28 +4,37 @@ namespace ConsoleApp
 {
     class Program
     {
+
+        private static readonly int SnakeStateTotal = 3;
+        private const int SnakeState1 = 0;
+        private const int SnakeState2 = 1;
+        private const int SnakeState3 = 2;
+        private static readonly int SnakeMoveCount= 50;
+        private static readonly int SnakeRow = 5;
+        private static readonly int SnakeSleepTime;
+
         static void Main(string[] args)
         {
-            int x = 1;
-            while (x<50)
+            int x = SnakeState1;
+            while (x< SnakeMoveCount)
             {
                 Console.Clear();
-                Console.SetCursorPosition(x, 5);
+                Console.SetCursorPosition(x, SnakeRow);
 
-                switch(x % 3)
+                switch(x % SnakeStateTotal)
                 {
-                    case 0:
+                    case SnakeState1:
                         Console.WriteLine("__@");
                         break;
-                    case 1:
+                    case SnakeState2:
                         Console.WriteLine("_^@");
                         break;
-                    case 2:
+                    case SnakeState3:
                         Console.WriteLine("@_^");
                         break;
                 }
                 // 100밀리초 정지하고 x 증가
-                Thread.Sleep(100);
+                Thread.Sleep(SnakeSleepTime);
                 x++;
             }
         }
