1bf10516a6eef183fcc0d534fb27612ebdc33da5
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-04-17 11:57:51 +0900

    ✨ : SetCursorPosition 연습

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            // 위치는 절대값
+            Console.Write("메서드 호출");
+            Console.SetCursorPosition(5, 5);
+            Console.Write("메서드 호출2");
+            Console.SetCursorPosition(2, 2);
+            Console.Write("메서드 호출3");
         }
     }
 }
